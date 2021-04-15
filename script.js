6c5d7e75507f303e088926127742364a0f5f7305
maxrabin <max.rabin17@gmail.com>
2021-04-15 11:51:05 -0400

    set font family and font size, began biography

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 const h2 = document.createElement('h2');
-h2.textContent = "This content added by JavaScript"; 
+h2.textContent = "Beginnger software engineer, \ninterested in building and creating personal websites."; 
 document.querySelector('body').appendChild(h2); 
 
 const h3 = document.createElement('h3')
-h3.textContent = "This is more text content added by Javascript";
+h3.textContent = "I am looking forward to embarking on a new journey as a website developer.";
 document.querySelector('body').appendChild(h3);
+
